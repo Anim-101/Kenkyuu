a41384f89167cbabe88dd77240fff64a80de6f3a
Anim-101 <anmaksh@gmail.com>
2021-08-03 22:25:42 +0600

    Stable mongo django init - Django-MongoDB.

--- Django-DBs/Django-MongoDB/app/djanmon/settings.py
+++ Django-DBs/Django-MongoDB/app/djanmon/settings.py
@@ -21,9 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'v=nc_uf@@=pw%na-1r4#tiez+o%svx!dnu1bhquz&=gg&v6_9!'
+os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
